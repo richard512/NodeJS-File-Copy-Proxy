5f7d9115f3f122b8a2b538c1950f5dc8c4274826
richard512 <richardt512@yahoo.com>
2016-09-08 01:35:21 -0700

    Update haunt.sh

--- haunt.sh
+++ haunt.sh
@@ -5,10 +5,10 @@
 for var in "$@"
 do
 	echo "Rendering on iPhone: $var"
-	phantomjs --ignore-ssl-errors=yes --proxy=localhost:8080 "iphone.js" "$var"
+	phantomjs --ignore-ssl-errors=yes --proxy=localhost:8080 "iphone.ph.js" "$var"
 
 	echo "Rendering on Desktop $var"
-	phantomjs --ignore-ssl-errors=yes --proxy=localhost:8080 "desktop.js" "$var"
+	phantomjs --ignore-ssl-errors=yes --proxy=localhost:8080 "desktop.ph.js" "$var"
 	sleep 2
 done
 
